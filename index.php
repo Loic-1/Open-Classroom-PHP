f02feee6ce9c8f8644c3b42dd868ceb93b01fd36
Loic-1 <loic.vasile130@gmail.com>
2024-07-30 11:08:37 +0200

    Show More ...

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 try {
     // On se connecte à MySQL
-    $mysqlClient = new PDO('mysql:host=localhost;dbname=partage_de_recettes;charset=utf8', 'root', '');
+    $mysqlClient = new PDO(
+        'mysql:host=localhost;dbname=partage_de_recettes;charset=utf8',
+        'root',
+        '',
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],
+    );
 } catch (Exception $e) {
     // En cas d'erreur, on affiche un message et on arrête tout
     die('Erreur : ' . $e->getMessage());
@@ -10,9 +15,13 @@ try {
 
 // On récupère tout le contenu de la table recipes
 
-$sqlQuery = 'SELECT * FROM recipes WHERE is_enabled = TRUE';
+$sqlQuery = 'SELECT * FROM recipes WHERE author = :author AND is_enabled = :is_enabled';
+
 $recipesStatement = $mysqlClient->prepare($sqlQuery);
-$recipesStatement->execute();
+$recipesStatement->execute([
+    'author' => 'mathieu.nebra@exemple.com',
+    'is_enabled' => true,
+]);
 $recipes = $recipesStatement->fetchAll();
 
 // On affiche chaque recette une à une
