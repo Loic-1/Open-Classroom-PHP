8790fc0dcf00bd8b1ad4ec98fe3f2e0e78a49cfb
Loic-1 <loic.vasile130@gmail.com>
2024-07-30 11:27:58 +0200

    Show More ...

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ try {
 
 // On récupère tout le contenu de la table recipes
 
-$sqlQuery = 'SELECT * FROM recipes WHERE author = :author AND is_enabled = :is_enabled';
+$sqlQuery = 'SELECT title FROM recipes WHERE author = :author AND is_enabled = :is_enabled';
 
 $recipesStatement = $mysqlClient->prepare($sqlQuery);
 $recipesStatement->execute([
@@ -27,7 +27,7 @@ $recipes = $recipesStatement->fetchAll();
 // On affiche chaque recette une à une
 foreach ($recipes as $recipe) {
 ?>
-    <p><?php echo $recipe['author']; ?></p>
+    <p><?php echo $recipe['title']; ?></p>
 <?php
 }
 ?>
